9a605bca71fb34e9e104d75c3f2e6a8b4852c622
cyoce <masterryangp@gmail.com>
2023-11-02 14:24:42 -0700

    hp print works

--- api_calls.js
+++ api_calls.js
@@ -1,1 +1,7 @@
 console.log("api calls");
+(async function main() {
+    const Poke = new Pokedex.Pokedex();
+    const snorlax = await Poke.getPokemonByName("snorlax");
+    window.snorlax = snorlax;
+    console.log(`Snorlax HP: ${snorlax.stats.find(stat => stat.stat.name == "hp").base_stat }`);
+})();
